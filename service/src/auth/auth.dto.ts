f3ef3e7e4d5e4462c5d7b945ebb48a8bee92a33d
pengmeng <pengmeng@qcc.com>
2026-02-11 16:33:11 +0800

    校验逻辑

--- service/src/auth/auth.dto.ts
+++ service/src/auth/auth.dto.ts
@@ -1,12 +1,14 @@
-import { IsString, MinLength } from 'class-validator';
+import { IsString, IsNotEmpty, Length } from 'class-validator';
 
 export class RegisterDto {
-  @IsString()
+  @IsString({ message: '用户名必须是字符串' })
+  @IsNotEmpty({ message: '用户名不能为空' })
+  @Length(3, 20, { message: '用户名长度 3-20 位' })
   name: string;
 
   @IsString()
   email: string;
 
-  @MinLength(6)
+  @Length(6, 32, { message: '密码长度 6-32 位' })
   password: string;
 }
--- service/src/main.ts
+++ service/src/main.ts
@@ -8,10 +8,12 @@ if (!global.crypto) {
 
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
